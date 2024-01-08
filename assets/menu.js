969c46879d761047799e5d95ae0044bc69fc5081
Keegan Royal-Eisenberg <kroyal.eisenberg@gmail.com>
2024-01-07 23:14:59 -0800

    Trying new Javascript to capture touchscreen input.

--- assets/menu.js
+++ assets/menu.js
@@ -1,7 +1,14 @@
+// $(document).ready(function(){
+
+//     $(".burger-nav").on("click", function(){
+
+//         $(".header nav ul").toggleClass("open");
+//     });
+// })
+
 $(document).ready(function(){
-
-    $(".burger-nav").on("click", function(){
-
+    var $burgerNav = $(".burger-nav");
+    $burgerNav.on("click touchstart", function(){
         $(".header nav ul").toggleClass("open");
     });
-})
+ });
