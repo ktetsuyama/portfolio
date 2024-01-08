f33580a7f1cab0ba05f83ef831cc1fddd18f5364
Keegan Royal-Eisenberg <kroyal.eisenberg@gmail.com>
2024-01-07 23:26:22 -0800

    Trying to add a slight delay

--- assets/menu.js
+++ assets/menu.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 
     $(".burger-nav").on("click", function(){
-
-        $(".header nav ul").toggleClass("open");
+        setTimeout(function(){
+            $(".header nav ul").toggleClass("open");
+        }, 10);
     });
 })
