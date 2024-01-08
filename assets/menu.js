a495f86893407d4a6adf8080ff002464bd5c2201
Keegan Royal-Eisenberg <kroyal.eisenberg@gmail.com>
2024-01-07 23:19:49 -0800

    Removed new Javascript as it didn't work.

--- assets/menu.js
+++ assets/menu.js
@@ -1,14 +1,7 @@
-// $(document).ready(function(){
-
-//     $(".burger-nav").on("click", function(){
-
-//         $(".header nav ul").toggleClass("open");
-//     });
-// })
-
 $(document).ready(function(){
-    var $burgerNav = $(".burger-nav");
-    $burgerNav.on("click touchstart", function(){
+
+    $(".burger-nav").on("click", function(){
+
         $(".header nav ul").toggleClass("open");
     });
- });
+})
