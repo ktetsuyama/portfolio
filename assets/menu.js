9facb8511407a03e8abbd1da7b297aef0ea30cca
Keegan Royal-Eisenberg <kroyal.eisenberg@gmail.com>
2024-01-07 23:21:00 -0800

    Trying vanilla Javascript approach.

--- assets/menu.js
+++ assets/menu.js
@@ -1,7 +1,15 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-    $(".burger-nav").on("click", function(){
+//     $(".burger-nav").on("click", function(){
 
-        $(".header nav ul").toggleClass("open");
+//         $(".header nav ul").toggleClass("open");
+//     });
+// })
+
+document.addEventListener("DOMContentLoaded", function() {
+    var burgerNav = document.querySelector(".burger-nav");
+    burgerNav.addEventListener("click", function(){
+        var headerNavUl = document.querySelector(".header nav ul");
+        headerNavUl.classList.toggle("open");
     });
-})
+ });
